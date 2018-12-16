ad4931dd299b44f3e65ef61493f2fdad4442e07e
GaryBirges <gary.birges@m3marketaccess.com>
2018-12-16 15:03:03 +0000

    snake working solution

--- src/app/games/snake/snake.component.ts
+++ src/app/games/snake/snake.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-snake',
@@ -9,27 +10,32 @@ export class SnakeComponent implements OnInit {
   state
   canvas 
   ctx
-  // Position helpers
-  x = c => Math.round(c * this.canvas.width / this.state.cols)
-  y = r => Math.round(r * this.canvas.height / this.state.rows)
+ 
   constructor() { }
 
   ngOnInit() {
-    // this.state = this.initialState()
-    // this.canvas= document.getElementById('canvas') as HTMLCanvasElement;
-    // this.ctx = this.canvas.getContext('2d');
-    // this.draw(); 
-    // window.requestAnimationFrame(this.step(0))
-     this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
     this.ctx = this.canvas.getContext('2d')
-    console.log(this.ctx)
-    this.state = initialState()
+    this.state = this.initialState()
     this.draw();  
     window.requestAnimationFrame(this.step(0))
   }
 
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) { 
+    switch (event.key) {
+      case 'w': case 'h': case 'ArrowUp':    this.state = this.enqueue(this.state, NORTH); break
+      case 'a': case 'j': case 'ArrowLeft':  this.state = this.enqueue(this.state, WEST);  break
+      case 's': case 'k': case 'ArrowDown':  this.state = this.enqueue(this.state, SOUTH); break
+      case 'd': case 'l': case 'ArrowRight': this.state = this.enqueue(this.state, EAST);  break
+    }
+  }
+ // Position helpers
+ x = c => Math.round(c * this.canvas.width / this.state.cols)
+ y = r => Math.round(r * this.canvas.height / this.state.rows)
+
   // Game loop draw
-draw = () => {
+draw() {
   // clear
   this.ctx.fillStyle = '#232323'
   this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
@@ -51,7 +57,7 @@ draw = () => {
 
 step = t1 => t2 => {
   if (t2 - t1 > 100) {
-    this.state = next(this.state)
+    this.state = this.next(this.state)
     this.draw()
     window.requestAnimationFrame(this.step(t2))
   } else {
@@ -59,224 +65,60 @@ step = t1 => t2 => {
   }
 }
 
+enqueue (state, move){
+ return this.validMove(move)(state)
+ ? merge(state)({ moves: state.moves.concat([move]) })
+ : state
+} 
+
+// Point operations
+pointEq = p1 => p2 => p1.x == p2.x && p1.y == p2.y
+
+willEat   = state => this.pointEq(this.nextHead(state))(state.apple)
+willCrash = state => state.snake.find(this.pointEq(this.nextHead(state)))
+
+validMove = move => state =>
+  state.moves[0].x + move.x != 0 || state.moves[0].y + move.y != 0
+
+// Next values based on state
+ nextMoves = state => state.moves.length > 1 ? dropFirst(state.moves) : state.moves
+ nextApple = state => this.willEat(state) ? this.rndPos(state) : state.apple
+ nextHead  = state => state.snake.length == 0
+  ? { x: 2, y: 2 }
+  : {
+    x: mod(state.cols)(state.snake[0].x + state.moves[0].x),
+    y: mod(state.rows)(state.snake[0].y + state.moves[0].y)
+  }
+ nextSnake = state => this.willCrash(state)
+  ? []
+  : (this.willEat(state)
+    ? [this.nextHead(state)].concat(state.snake)
+    : [this.nextHead(state)].concat(dropLast(state.snake)))
 
 
-  // functions
-
-// mod(n, m) {
-//   return ((n % m) + m) % m;
-// }
-
-//  spec      = o => x => Object.keys(o)
-//   .map(k => this.objOf(k)(o[k](x)))
-//   .reduce((acc, o) => Object.assign(acc, o))
-
-//  objOf     = k => v => { var o = {}; o[k] = v; return o }
-
-//  prop      = k => o => o[k]
-
-//  merge     = o1 => o2 => Object.assign({}, o1, o2)
-// /********************************************************************************** snake */
-// //   const base = require('./base')
-// // Object.getOwnPropertyNames(base).map(p => global[p] = base[p])
-
-// // Constants
-//  NORTH = { x: 0, y:-1 }
-//  SOUTH = { x: 0, y: 1 }
-//  EAST  = { x: 1, y: 0 }
-//  WEST  = { x:-1, y: 0 }
-
-// // Point operations
-//  pointEq = p1 => p2 => p1.x == p2.x && p1.y == p2.y
-
-// // Booleans
-//  willEat(state){
-//   return this.pointEq(this.nextHead(state))(state.apple)
-   
-//  }
-
-
-//  willCrash(state)  {
-//   return  state.snake.find(this.pointEq(this.nextHead(state)))
-  
-//  } 
-
-
-//  validMove( move,  state) {
-//   return state.moves[0].x + move.x != 0 || state.moves[0].y + move.y != 0
+  rndPos = table => ({
+    x: rnd(0)(table.cols - 1),
+    y: rnd(0)(table.rows - 1)
+  })
     
-//  }
-
-// // Next values based on state
-
-//  nextMoves(state){
-//   return state.moves.length > 1 ?  state.moved.slice(1): state.moves
-
-//  } 
-//  /*dropFirst(state.moves) : state.moves*/
-
-//  nextApple(state) {
-//   return this.willEat(state) ? this.rndPos(state) : state.apple
-//  } 
-//  nextHead  = state => state.snake.length == 0
-//   ? { x: 2, y: 2 }
-//   : {
-//     x: this.mod(state.cols,state.snake[0].x + state.moves[0].x),
-//     y: this.mod(state.rows,state.snake[0].y + state.moves[0].y)
-//   }
-//  nextSnake = state => this.willCrash(state)
-//   ? []
-//   : (this.willEat(state)
-//     ? [this.nextHead(state)].concat(state.snake)                                              
-//     : [this.nextHead(state)].concat(state.snake.slice(0,state.snake.length-1 )))          //droplast
-
-// // Randomness
-//  rndPos = table => ({
-//   x:  Math.floor(Math.random() * (table.cols - 1)) + 0,
-//   y:  Math.floor(Math.random() * (table.rows - 1)) + 0,
-// })
-
-// // Initial state
-//  initialState = () => ({
-//   cols:  20,
-//   rows:  14,
-//   moves: [this.EAST],
-//   snake: [],
-//   apple: { x: 16, y: 2 },
-// })
-
-//  next = this.spec({
-//   rows:  this.prop('rows'),
-//   cols:  this.prop('cols'),
-//   moves: this.nextMoves,
-//   snake: this.nextSnake,
-//   apple: this.nextApple
-// })
-
-//  enqueue = (state, move) => this.validMove(move,state)
-//   ? this.merge(state)({ moves: state.moves.concat([move]) })
-//   : state
-
-// // module.exports = { EAST, NORTH, SOUTH, WEST, initialState, enqueue, next, }
-
-// /********************************************************web */
-
-
-// // Mutable state
-// // let state = this.initialState()
-
-// // Position helpers
-//  x = c => Math.round(c * this.canvas.width / this.state.cols)
-//  y = r => Math.round(r * this.canvas.height / this.state.rows)
-
-// // Game loop draw
-//  draw = () => {
-//   // clear
-//   this.ctx.fillStyle = '#232323'
-//   this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
-
-//   // draw snake
-//   this.ctx.fillStyle = 'rgb(0,200,50)'
-//   this.state.snake.map(p => this.ctx.fillRect(this.x(p.x), this.y(p.y), this.x(1), this.y(1)))
-
-//   // draw apples
-//   this.ctx.fillStyle = 'rgb(255,50,0)'
-//   this.ctx.fillRect(this.x(this.state.apple.x), this.y(this.state.apple.y), this.x(1), this.y(1))
-
-//   // add crash
-//   if (this.state.snake.length == 0) {
-//     this.ctx.fillStyle = 'rgb(255,0,0)'
-//     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
-//   }
-// }
-
-// // Game loop update
-//  step = t1 => t2 => {
-//   if (t2 - t1 > 100) {
-//     this.state = this.next(this.state)
-//     this.draw()
-//     window.requestAnimationFrame(this.step(t2))
-//   } else {
-//     window.requestAnimationFrame(this.step(t1))
-//   }
-// }
-
-// Key events
-// window.addEventListener('keydown', e => {
-//   switch (e.key) {
-//     case 'w': case 'h': case 'ArrowUp':    state = enqueue(state, NORTH); break
-//     case 'a': case 'j': case 'ArrowLeft':  state = enqueue(state, WEST);  break
-//     case 's': case 'k': case 'ArrowDown':  state = enqueue(state, SOUTH); break
-//     case 'd': case 'l': case 'ArrowRight': state = enqueue(state, EAST);  break
-//   }
-// })
-
-// Main
-// this.draw(); 
-// window.requestAnimationFrame(this.step(0))
-
-
+  // Initial state
+    initialState = () => ({
+    cols:  20,
+    rows:  14,
+    moves: [EAST],
+    snake: [],
+    apple: { x: 16, y: 2 },
+  })
+  
+    next = spec({
+    rows:  prop('rows'),
+    cols:  prop('cols'),
+    moves: this.nextMoves,
+    snake: this.nextSnake,
+    apple: this.nextApple
+  })
 
 }
-
-// const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-// const ctx = canvas.getContext('2d')
-
-// Mutable state
-
-
-// Position helpers
-// x = c => Math.round(c * this.canvas.width / this.state.cols)
-// y = r => Math.round(r * this.canvas.height / this.state.rows)
-
-// Game loop draw
-// draw = () => {
-//   // clear
-//   this.ctx.fillStyle = '#232323'
-//   this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
-
-//   // draw snake
-//   this.ctx.fillStyle = 'rgb(0,200,50)'
-//   this.state.snake.map(p => this.ctx.fillRect(x(p.x), y(p.y), x(1), y(1)))
-
-//   // draw apples
-//   this.ctx.fillStyle = 'rgb(255,50,0)'
-//   this.ctx.fillRect(x(this.state.apple.x), y(this.state.apple.y), x(1), y(1))
-
-//   // add crash
-//   if (this.state.snake.length == 0) {
-//     this.ctx.fillStyle = 'rgb(255,0,0)'
-//     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
-//   }
-// }
-
-// Game loop update
-// const step = t1 => t2 => {
-//   if (t2 - t1 > 100) {
-//     this.state = next(this.state)
-//     draw()
-//     window.requestAnimationFrame(step(t2))
-//   } else {
-//     window.requestAnimationFrame(step(t1))
-//   }
-// }
-
-// Key events
-window.addEventListener('keydown', e => {
-  switch (e.key) {
-    case 'w': case 'h': case 'ArrowUp':    this.state = enqueue(this.state, NORTH); break
-    case 'a': case 'j': case 'ArrowLeft':  this.state = enqueue(this.state, WEST);  break
-    case 's': case 'k': case 'ArrowDown':  this.state = enqueue(this.state, SOUTH); break
-    case 'd': case 'l': case 'ArrowRight': this.state = enqueue(this.state, EAST);  break
-  }
-})
-
-// Main
-// draw(); window.requestAnimationFrame(step(0))
-
-
-
-
 
 const adjust    = n => f => xs => mapi(x => i => i == n ? f(x) : x)(xs)
 const dropFirst = xs => xs.slice(1)
@@ -297,65 +139,7 @@ const spec      = o => x => Object.keys(o)
   .map(k => objOf(k)(o[k](x)))
   .reduce((acc, o) => Object.assign(acc, o))
 
-// module.exports = { adjust, dropFirst, dropLast, id, k, map, merge, mod, objOf, pipe, prop, range, rep, rnd, spec }
-
-// Object.getOwnPropertyNames(base).map(p => global[p] = base[p])
-
-// Constants
 const NORTH = { x: 0, y:-1 }
 const SOUTH = { x: 0, y: 1 }
 const EAST  = { x: 1, y: 0 }
 const WEST  = { x:-1, y: 0 }
-
-// Point operations
-const pointEq = p1 => p2 => p1.x == p2.x && p1.y == p2.y
-
-// Booleans
-const willEat   = state => pointEq(nextHead(state))(state.apple)
-const willCrash = state => state.snake.find(pointEq(nextHead(state)))
-const validMove = move => state =>
-  state.moves[0].x + move.x != 0 || state.moves[0].y + move.y != 0
-
-// Next values based on state
-const nextMoves = state => state.moves.length > 1 ? dropFirst(state.moves) : state.moves
-const nextApple = state => willEat(state) ? rndPos(state) : state.apple
-const nextHead  = state => state.snake.length == 0
-  ? { x: 2, y: 2 }
-  : {
-    x: mod(state.cols)(state.snake[0].x + state.moves[0].x),
-    y: mod(state.rows)(state.snake[0].y + state.moves[0].y)
-  }
-const nextSnake = state => willCrash(state)
-  ? []
-  : (willEat(state)
-    ? [nextHead(state)].concat(state.snake)
-    : [nextHead(state)].concat(dropLast(state.snake)))
-
-// Randomness
-const rndPos = table => ({
-  x: rnd(0)(table.cols - 1),
-  y: rnd(0)(table.rows - 1)
-})
-
-// Initial state
-const initialState = () => ({
-  cols:  20,
-  rows:  14,
-  moves: [EAST],
-  snake: [],
-  apple: { x: 16, y: 2 },
-})
-
-const next = spec({
-  rows:  prop('rows'),
-  cols:  prop('cols'),
-  moves: nextMoves,
-  snake: nextSnake,
-  apple: nextApple
-})
-
-const enqueue = (state, move) => validMove(move)(state)
-  ? merge(state)({ moves: state.moves.concat([move]) })
-  : state
-
-// module.exports = { EAST, NORTH, SOUTH, WEST, initialState, enqueue, next, }
