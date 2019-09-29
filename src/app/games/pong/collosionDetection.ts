eb9ae3ffafb8577f57fca82b2ad4b703e2833cec
GaryBirges <gary.birges@m3marketaccess.com>
2019-09-29 12:10:29 +0100

    pong: ballcan enter from the sides bug found and corrected

--- src/app/games/pong/collosionDetection.ts
+++ src/app/games/pong/collosionDetection.ts
@@ -1,6 +1,8 @@
 export function detectCollosion(ball, gameElement){
     let ballBottom = ball.position.y+ball.size
     let ballTop = ball.position.y
+    let ballLeft =  ball.position.x 
+    let ballRight = ball.position.x + ball.size
 
     let elementTop = gameElement.position.y
     let elementLeft = gameElement.position.x
@@ -8,13 +10,18 @@ export function detectCollosion(ball, gameElement){
     let elementBottom = gameElement.position.y +gameElement.height;
 
     if(ballBottom >= elementTop && 
-        ballTop<=elementBottom &&
-        ball.position.x>= elementLeft && 
-        ball.position.x + ball.size <= elementRight){
+        ballTop <= elementBottom &&
+        ballRight >= elementLeft && 
+        ballLeft <= elementRight){
+            // console.log("fromTop",Math.abs(ballBottom - elementTop))
+            // console.log('fromBottom',Math.abs(ballTop - elementBottom))
+            // console.log('fromRight',Math.abs(ballRight - elementLeft)) 
+            // console.log('FromLeft',Math.abs(ballLeft - elementRight))
+            // console.log("fromTop, fromBottom, fromRight, FromLeft")
+            // console.log(ballBottom, ballTop,      ballLeft, ballRight)
+            // console.log(elementTop, elementBottom, elementRight, elementLeft  )
             return true;
     }else{
         return false;
     }
-            
-
 }
